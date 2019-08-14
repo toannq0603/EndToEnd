9f757dbfebe3a374e1ca761fd93834e92154a6ac
toannq06031999 <toannq06031999@gmail.com>
2019-08-14 15:06:04 +0700

    Add project files.

---
+++ EndToEnd/DAL/SchoolConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure.Interception;
+using System.Data.Entity.SqlServer;
+
+namespace EndToEnd.DAL
+{
+    public class SchoolConfiguration : DbConfiguration
+    {
+        public SchoolConfiguration()
+        {
+            SetExecutionStrategy("System.Data.SqlClient", () => new SqlAzureExecutionStrategy());
+            DbInterception.Add(new SchoolInterceptorTransientErrors());
+            DbInterception.Add(new SchoolInterceptorLogging());
+        }
+    }
+}
